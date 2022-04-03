10c2a14bc62e6c0e3557732422b5434156c444a1
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-04-03 15:35:19 +0200

    Setting up navigation

---
+++ screen/Home.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Text, View, SafeAreaView, FlatList } from "react-native";
+
+import { COLORS, NFTData } from "../constants";
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <Text>Home</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
