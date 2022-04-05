c0359559c00ed4e65983baaa8907cdcd5c16dde1
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-04-05 15:36:44 +0200

    Changing the header

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -52,7 +52,7 @@ const HomeHeader = ({ onSearch }) => {
             color: COLORS.white,
           }}
         >
-          Hello Victoria 👋
+          Hello Arsim 👋
         </Text>
 
         <Text
